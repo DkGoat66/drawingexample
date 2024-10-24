8a241e70077fbc7434f61b84a206b5ef54ce1582
Diwakar Rai <111923854+DkGoat66@users.noreply.github.com>
2024-10-24 14:31:01 -0400

    Update drawingexample.dart

--- drawingexample.dart
+++ drawingexample.dart
@@ -4,6 +4,7 @@ import "package:flutter/material.dart";
 import 'package:path_drawing/path_drawing.dart';
 // Define a triangle path using SVG path data
 final trianglePath = parseSvgPathData('M200 0 L75 200 L225 200 Z');
+// Main widget for the drawing example
 class DrawingExample extends StatelessWidget {
   const DrawingExample({super.key});
   
