cb74859c8e2a7e3ab60b9017647092a3f1bdb637
Diwakar Rai <111923854+DkGoat66@users.noreply.github.com>
2024-10-27 22:52:14 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
 //Import necessary packages
 import 'package:drawingexample/drawingexample.dart';
 import 'package:flutter/material.dart';
-
+// Entry point of the application
 void main() {
   runApp(const DrawingExample());
 }
